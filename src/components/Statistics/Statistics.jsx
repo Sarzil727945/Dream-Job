fb3dc8f3a6738eba478b94d5d85d44f0c02772c3
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-04-09 22:43:24 +0600

    header part complete

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import './Statistics.css'
 const Statistics = () => {
      return (
           <div className=' mt-5 pt-5'>
-               {/* <header className='container py-lg-5'>
+               <header className='container py-lg-5'>
                     <div className='row'>
                          <div className='col-lg-6 my-4'>
                               <h1 className='display-4 fw-bold'>One Step <br /> Closer To Your <br /> <span className='text-info'>Dream Job</span></h1>
@@ -16,7 +16,7 @@ const Statistics = () => {
                               <img src="../../../public/img3.png" className=' img-fluid imgStyle' alt="" />
                          </div>
                     </div>
-               </header> */}
+               </header>
           {/* header part end  */}
                <section>
 
