acd52bc9e8d3440ff2bd711986c1f4e372fb8260
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-04-10 21:49:04 +0600

    Job Details part complete

--- src/components/FeaturedJob/FeaturedJob.jsx
+++ src/components/FeaturedJob/FeaturedJob.jsx
@@ -3,22 +3,25 @@ import { Button, Card } from 'react-bootstrap';
 import { Link} from 'react-router-dom';
 
 const FeaturedJob = ({ jobData }) => {
-     const { logo, name, location, salary, id } = jobData;
+     const { logo, jobTitle, location, salary, id, name } = jobData;
      return (
           <div className='col-lg-6 mb-3'>
                <div>
                     <Card className=''>
                          <Card.Body>
                               <img className='CartImgStyle' src={logo} alt="" />
-                              <Card.Title className=''>{name}</Card.Title>
+                              <Card.Title className='mt-4'>{jobTitle}</Card.Title>
+                              <Card.Subtitle className=" text-muted mb-2">
+                                   {name}
+                              </Card.Subtitle>
                               <Button variant="outline-info" className='m-2'>Remote</Button>
                               <Button variant="outline-info" className='m-2'>Full Time</Button>
                               <Card.Subtitle className=" text-muted my-2">
                                    <span className='me-3'>{location}</span>
-                                   <span> {salary}</span>
+                                   <span> Salary :  {salary}</span>
                               </Card.Subtitle>
-                              <Button variant="info">
-                                   <Link to={`/viewDetails/${id}`}>View Details</Link>
+                              <Button  variant="info">
+                                   <Link className='text-decoration-none' to={`/viewDetails/${id}`}>View Details</Link>
                               </Button>
                          </Card.Body>
                     </Card>
--- src/components/ViewDetails/ViewDetails.jsx
+++ src/components/ViewDetails/ViewDetails.jsx
@@ -1,21 +1,79 @@
 import React, { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const ViewDetails = () => {
-     const jsonId = useParams();
-     console.log(jsonId);
+     const { id } = useParams();
 
+     // const [idData, setIdData] = useState([])
      const [jobJsonData, setJobJsonData] = useState([]);
-     useEffect(()=> {
-          fetch('/FeaturedJob.json')
-          .then(res => res.json())
-          .then(data => setJobJsonData(data))
-     },[])
-     console.log(jobJsonData)
+     // useEffect(() => {
+     //      fetch('/FeaturedJob.json')
+     //           .then(res => res.json())
+     //           .then(data => setJobJsonData(data))
+
+     //      if (id) {
+     //           const foundProduct = jobJsonData.find(pd => pd.id === +id)
+     //           setIdData(foundProduct);
+     //      }
+
+     // }, [jobJsonData])
+
+     const productsAndCartData = async () => {
+          const productsData = await fetch('/FeaturedJob.json')
+          const products = await productsData.json()
+
+          if (id) {
+               const foundProduct = products.find(product => product.id === +id)
+               setJobJsonData(foundProduct)
+          }
+
+     }
+     productsAndCartData()
+     const { jDescription, jResponsibility, eRequirements, Experiences, salary, jobTitle, Phone, Email, Address, name } = jobJsonData
+
 
      return (
-          <div className='mt-5 pt-5'>
-               <h1>/viewDetails</h1>
+          <div className='my-5 pt-5 container'>
+               <h1 className='my-5 py-5 text-center'>Job Details</h1>
+               <div className=' row my-2'>
+                    <div className='col-lg-8'>
+                         <p><b>Job Description: </b>{jDescription}</p>
+                         <p><b>Job Responsibility: </b>{jResponsibility}</p>
+                         <p><b>Educational Requirements:</b></p>
+                         <p>{eRequirements}</p>
+                         <p><b>Experiences:</b></p>
+                         <p>{Experiences}</p>
+
+                    </div>
+                    <div className='col-lg-4'>
+                         <div className="card text-bg-info mb-3">
+                              <div className="card-header"><b>Job Details {id}
+                              </b>
+                              </div>
+                              <div className="card-body">
+                                   <p className="card-text"><b>Salary: </b>
+                                        {salary} (Per Month)
+                                   </p>
+                                   <p><b>Job Title: </b> {jobTitle}</p>
+                              </div>
+                              <div className="card-header"><b>Contact Information
+                              </b>
+                              </div>
+                              <div className="card-body">
+                                   <p className="card-text"><b>Phone: </b>
+                                        {Phone}
+                                   </p>
+                                   <p><b>Email: </b> {Email}</p>
+                                   <p><b>Address: </b> {Address}</p>
+                              </div>
+                         </div>
+                         <div class="d-grid mx-auto">
+                              <button class="btn btn-primary" type="button">Apply Now</button>
+                         </div>
+                    </div>
+
+               </div>
           </div>
      );
 };
