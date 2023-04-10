e836e392f6994814a3f6ccca800b5986a8bfd2b9
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-04-11 00:08:28 +0600

    local Storage data add

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { addToDb, getStoredCart } from '../../utils/fakeDB';
 
 const Statistics = () => {
      const { id } = useParams();
-     console.log(id)
+     addToDb(id);
+     
+
+     const [lcData, setLcData] = useState([]);
+     const productsAndCartData = async () => {
+          const productsData = await fetch('/FeaturedJob.json')
+          const products = await productsData.json()
+          const savedCart = getStoredCart()
+          const initialCart = []
+          for (const id in savedCart) {
+               const foundProduct = products.find(product => product.id === +id)
+               if (foundProduct) {
+                 const quantity = savedCart[id]
+                 foundProduct.quantity = quantity
+                 initialCart.push(foundProduct)
+               }
+             }
+             setLcData(initialCart)
+     }
+     productsAndCartData()
+
+     console.log(lcData);
+
      return (
           <div className='mt-5 pt-5'>
               <h1>Statistics</h1> 
