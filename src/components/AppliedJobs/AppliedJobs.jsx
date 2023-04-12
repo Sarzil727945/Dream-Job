4ca8a7058cdc32c5097b59a385d3d66a1e8a327a
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-04-12 09:02:10 +0600

    onsite and remote button complete

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -27,17 +27,32 @@ const AppliedJobs = () => {
      }
      productsAndCartData()
 
+
+     // Onsite and Remote button handel Start 
+     const [dataArr, setDataArr] = useState([])
+     const remoteRemote = (id) => {
+          if (id === 'Onsite') {
+               const onsite = lcData.filter(data => data.btn1 === id)
+               setDataArr(onsite)
+          }
+          else if (id) {
+               const remote = lcData.filter(data => data.btn1 === id)
+               setDataArr(remote)
+          }
+     }
+     const allDataArr = dataArr.length !== 0 ? dataArr : lcData;
+     // Onsite and Remote button handel end 
+
      return (
           <div className='mt-5 pt-5'>
                <h1 className='text-center my-5'>Applied Jobs</h1>
                <div className='d-flex justify-content-end container my-4'>
-                    <Button variant="info" className='my-1'>Onsite</Button>
-                    <Button variant="info" className='my-1 ms-3'>Remote</Button>
+                    <Button onClick={() => remoteRemote('Onsite')} variant="info" className='my-1'>Onsite</Button>
+                    <Button onClick={() => remoteRemote('Remote')} variant="info" className='my-1 ms-3'>Remote</Button>
                </div>
-               {/* <Button variant="outline-info" className='my-1 ms-2'>{btn2}</Button> */}
                <div>
                     {
-                         lcData.map(data => <AppliedData
+                         allDataArr.map(data => <AppliedData
                               key={data.id}
                               data={data}
                          ></AppliedData>)
